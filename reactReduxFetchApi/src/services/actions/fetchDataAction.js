5cbbf0bc513f0ae63dafe5000ccd7ba86ccd37ab
Mostafijur <mrmoon025@gmail.com>
2023-09-01 13:31:12 +0600

    [feat] Created action

--- reactReduxFetchApi/src/services/actions/fetchDataAction.js
+++ reactReduxFetchApi/src/services/actions/fetchDataAction.js
@@ -1,7 +1,19 @@
+import { FAILl_REQUEST, GET_REQUEST, SUCCESS_REQUEST, URL } from "../constants/fetchDataConstants"
+
 const fetchData = ()=> async(disptch) => {
+    disptch({type: GET_REQUEST});
     try {
+       const response = await axios.get(URL);
+       disptch({
+        type: SUCCESS_REQUEST,
+        payload: response.data
+       })
         
     } catch (error) {
-        
+        disptch({
+            type: FAILl_REQUEST,
+            payload: error.message 
+        })
     }
 }
+export default fetchData
