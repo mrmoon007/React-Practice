5bc6b4cff7867b307818108d53e0891440145e86
Mostafijur <mrmoon025@gmail.com>
2023-09-03 23:29:15 +0600

    [feat] Implemented higher order component pattern with class component

--- rho/src/components/HOC/withCounter.jsx
+++ rho/src/components/HOC/withCounter.jsx
@@ -1,15 +1,30 @@
 import React, { useState } from "react";
 
 const withCounter = (OriginalComponent) => {
+  // const NewComponent = () => {
+  //     const [count, setCount] = useState(0); // Initialize count to 0
+  //     const handleCount = () => {
+  //         setCount(count + 1);
+  //     }
+  //     return <OriginalComponent handleCount={handleCount} count={count} />
+  // }
+  // return NewComponent;
 
-    const NewComponent = () => {
-        const [count, setCount] = useState(0); // Initialize count to 0
-        const handleCount = () => {
-            setCount(count + 1);
-        }
-        return <OriginalComponent handleCount={handleCount} count={count} />
+  class NewComponent extends React.Component {
+    state = {
+      count: 0,
+    };
+
+    handleCount = () => {
+      this.setState((prevState) => ({ count: prevState.count + 1 }));
+    };
+
+    render() {
+      const { count } = this.state;
+      return <OriginalComponent count={count} handleCount={this.handleCount} />;
     }
-    return NewComponent;
-}
+  }
+  return NewComponent;
+};
 
 export default withCounter;
