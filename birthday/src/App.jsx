6216f8145c48bae448f96e8638458b18d365b004
Mostafijur <mrmoon025@gmail.com>
2023-09-13 00:29:26 +0600

    [style] Designed clear all button

--- birthday/src/App.jsx
+++ birthday/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
         <div className="container">
           <h3>5 Birthdays Today</h3>
           <List />
+          <button>Clear all</button>
         </div>
       </section>
     </main>
