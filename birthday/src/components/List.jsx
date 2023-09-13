6206f9023ef0b31f58e36c5851f46017d5065ffc
Mostafijur <mrmoon025@gmail.com>
2023-09-13 22:28:48 +0600

    [style] Designed list card

--- birthday/src/components/List.jsx
+++ birthday/src/components/List.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import img from '/img/person-1.jpeg'
 
 export default function List() {
   return (
-    <div>
-      list component
+    <div className='birthdayCard'>
+      <div>
+        <img src={img} alt="photo" />
+      </div>
+      <div>
+        <h4 className="title">Mr Moon</h4>
+        <p className="age">29 years</p>
+      </div>
     </div>
   )
 }
