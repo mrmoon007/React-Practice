fbb864d7d5bdae2dc5a588e8c2a61e5cfc103365
Mostafijur <mrmoon025@gmail.com>
2023-09-15 01:13:25 +0600

    [feat] Added react router

--- flexbox/src/App.jsx
+++ flexbox/src/App.jsx
@@ -1,5 +1,9 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Nav from './components/Nav'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contacts from './pages/Contacts'
 
 function App() {
 
@@ -7,6 +11,11 @@ function App() {
     <>
     <div>
       <Nav />
+      <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contacts />}></Route>
+        </Routes>
     </div>
     </>
   )
--- flexbox/src/components/Nav.jsx
+++ flexbox/src/components/Nav.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Nav() {
   return (
@@ -9,11 +10,11 @@ export default function Nav() {
         </div>
         <nav>
             <ul>
-                <li><a href="http://">Home</a></li>
-                <li><a href="http://">About</a></li>
-                <li><a href="http://">Contact us</a></li>
-                <li><a href="http://">FAQ</a></li>
-                <li><a href="http://">Sign Up</a></li>
+                <li><NavLink to="/home">Home</NavLink></li>
+                <li><NavLink to="/about">About</NavLink></li>
+                <li><NavLink to="/contact">Contact us</NavLink></li>
+                <li><NavLink to="/faq">FAQ</NavLink></li>
+                <li><NavLink to="/sign-up">Sign Up</NavLink></li>
             </ul>
         </nav>
       </header>
