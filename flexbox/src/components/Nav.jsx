5f5e8004bfa190bb650ff0820a6667379d9550e6
Mostafijur <mrmoon025@gmail.com>
2023-09-14 23:46:46 +0600

    [feat] Created header component

---
+++ flexbox/src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <>
+      <header>
+        <div>
+            <img src="" alt="logo" />
+        </div>
+        <nav>
+            <ul>
+                <li><a href="http://">Home</a></li>
+                <li><a href="http://">About</a></li>
+                <li><a href="http://">Contact us</a></li>
+                <li><a href="http://">FAQ</a></li>
+                <li><a href="http://">Sign Up</a></li>
+            </ul>
+        </nav>
+      </header>
+    </>
+  )
+}
